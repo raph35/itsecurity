d5ead67bd1d95f905631c97177aaace58863b342
Raphael RALAIKOA <raph35ral@gmail.com>
2020-11-27 13:44:40 +0300

    Last update

--- TP2/Exercice2/ploting.py
+++ TP2/Exercice2/ploting.py
@@ -1,16 +1,23 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import csv
 import matplotlib.pyplot as plt
-steelBlue = [0.2,0.5,1]
-yellow = [1,0.8,0.2]
 
-with open('output.csv', 'rb') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',', quotechar='|')
-    iter = reader.next()
-    timeaccess = reader.next()
-    timeaccessWithflush = reader.next()
+def plot(fileName):
+    with open(fileName, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_NONNUMERIC)
+        x = next(reader)
+        y = next(reader)
+        z = next(reader)
 
-plt.scatter(iter,timeaccess,c=yellow)
-plt.scatter(iter,timeaccessWithflush,c=steelBlue)
-plt.show()
+    fig = plt.figure()
+    ax = fig.add_axes([0.1,0.1,0.8,0.8])
+    ax.scatter(x,y, c="#D22B14")
+    ax.scatter(x,z, c="#4682b4")
+    ax.set_xlabel('iteration counter')
+    ax.set_ylabel('mesured access time')
+    ax.set_title('Time access of the pointer')
+    ax.legend(labels = ['without flush', 'with flush'], loc = 'upper right')
+    plt.show()
+
+plot('output.csv')
